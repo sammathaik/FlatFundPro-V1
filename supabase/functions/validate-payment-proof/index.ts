517117bc003985320065e3bd1237a674c0c84b70
sammathaik <sammathaik@gmail.com>
2025-12-17 11:43:40 +0530

    Updated index.ts

--- supabase/functions/validate-payment-proof/index.ts
+++ supabase/functions/validate-payment-proof/index.ts
@@ -256,7 +256,7 @@ function detectPaymentSignals(text: string): PaymentSignals {
 
   // Date detection
   const datePatterns = [
-    /\d{1,2}[\/-]\d{1,2}[\/-]\d{2,4}/,
+    /\d{1,2}[\/\-]\d{1,2}[\/\-]\d{2,4}/,
     /\d{4}-\d{2}-\d{2}/,
     /\d{1,2}\s+(?:Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)[a-z]*\s+\d{2,4}/i,
   ];
