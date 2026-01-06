c2cc0fa26f9d515da0c693fe653a1b99c50b97bf
sammathaik <sammathaik@gmail.com>
2026-01-06 16:33:47 +0530

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,14 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: 5173,
+    hmr: {
+      overlay: false,
+      timeout: 5000,
+    },
+    watch: {
+      usePolling: false,
+      interval: 1000,
+    },
   },
   preview: {
     host: '0.0.0.0',
