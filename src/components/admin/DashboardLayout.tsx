f0f5cf8e1798cdde2d506f02e53ac7148feab799
sammathaik <sammathaik@gmail.com>
2026-01-16 18:16:32 +0530

    Added flatfundpro-logo.svg

--- src/components/admin/DashboardLayout.tsx
+++ src/components/admin/DashboardLayout.tsx
@@ -74,7 +74,7 @@ export default function DashboardLayout({ children, activeTab, onTabChange, isSu
 
               {/* Logo - Subtle Brand Presence */}
               <img
-                src="/flatfundpro-2-logo.jpeg"
+                src="/flatfundpro-logo.svg"
                 alt="FlatFund Pro"
                 className="h-10 sm:h-11 object-contain flex-shrink-0"
               />
